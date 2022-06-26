fad74efadc7bb2279165f7e368f532744ef3b43f
mishaalnaeem <mishaalnaeem@gmail.com>
2022-06-26 16:52:36 +0500

    add analysis

--- driver.py
+++ driver.py
@@ -27,5 +27,12 @@ def main(argv):
     hurstexp = HurstExponent(data["Adj Close"].values, maxLag)
     print("Hurst exponent with lag ", maxLag, ": ", hurstexp)
 
+    if (hurstexp < 0.5):
+        print("Mean Reverting: High value will be followed by Low value next and vice versa")
+    elif (hurstexp == 0.5):
+        print("Geometric Random Walk: No clear deduction possible from given params")
+    else:
+        print("Trending: High value will be followed by High value and Low value by low")
+
 if __name__ == "__main__":
    main(sys.argv[1:])
